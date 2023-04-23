322bfd462962308f8891d63d7a6186ae5120ac78
Алексей <96423703+AlexFerji@users.noreply.github.com>
2023-04-23 19:19:01 +0300

    Update settings.py
    fix

--- appstorespy_test/settings.py
+++ appstorespy_test/settings.py
@@ -94,7 +94,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'appdb',
         'USER': 'root',
-        'PASSWORD': '394822sid',
+        'PASSWORD': '123456',
         'HOST': 'localhost',
         'PORT': '3306',
     }
